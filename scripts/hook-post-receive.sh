417b7de4ba06e25bb5a02e151a429bf6bc150cd5
andy <andy@sammalmaa.fi>
2024-07-17 21:20:56 +0300

    scripts: make hook self updating

--- scripts/hook-post-receive.sh
+++ scripts/hook-post-receive.sh
@@ -2,13 +2,16 @@
 
 set -eu
 
+gitref=$(cut -d" " -f 3 -)
+branch=$(echo "${gitref}" | cut -d/ -f3)
+
+# update the hook!
+git show "${branch}:scripts/hook-post-receive.sh" > hooks/post-receive
+
 rm -rf "$HOME/homelab"
 mkdir -p "$HOME/homelab"
 
 echo "==> Deploy Running"
-gitref=$(cut -d" " -f 3 -)
-branch=$(echo "${gitref}" | cut -d/ -f3)
-
 echo "    branch: ${branch}"
 
 git --work-tree="$HOME/homelab" checkout -f "${branch}"
