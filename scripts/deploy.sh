2fedd008e673875841551f277787609b98ab7b76
andy <andy@sammalmaa.fi>
2024-09-08 17:19:26 +0300

    deploy: only deploy vault for now

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@
 set -eu
 
 ./scripts/machine.sh "machines/vault"
-./scripts/machine.sh "machines/alpinecloud"
-./scripts/machine.sh "machines/minio"
+# ./scripts/machine.sh "machines/alpinecloud"
+# ./scripts/machine.sh "machines/minio"
